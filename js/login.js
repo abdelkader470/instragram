df2c8afea308714e04843648a93c0042fe18ac03
abdelkader470 <abdelkaderelsisy710@gmail.com>
2023-08-20 22:20:37 +0300

    before Validtion

--- js/login.js
+++ js/login.js
@@ -1,39 +1,34 @@
 const slider = document.querySelector(".slider-img");
-const btnLogon =document.querySelector('#btn-login')
-const errMessage = document.querySelector('.err-message')
-
+const btnLogon = document.querySelector("#btn-login");
+const errMessage = document.querySelector(".err-message");
 
 const changeBanner = () => {
   //Qisa yol ile sekli deyismek
   //Lakin animasiyali etmek ucun opacity clasi elave etmek lazim
   let current = 2;
   setInterval(() => {
-    slider.src = `http://127.0.0.1:5500/img/banner${current}.png`;
+    slider.src = `../images/banner1.png`;
     current != 4 ? current++ : (current = 1);
   }, 6000);
-}
+};
 const checkLoginBtn = () => {
   let username = document.querySelector(`#login-username`);
   let pass = document.querySelector(`#login-pass`);
   let btnLogin = document.querySelector(`#btn-login`);
-  username.value.length>5 && pass.value.length>5
-    ? btnLogin.disabled = false
-    : btnLogin.disabled = true;
-  console.log('check')
+  username.value.length > 5 && pass.value.length > 5
+    ? (btnLogin.disabled = false)
+    : (btnLogin.disabled = true);
+  console.log("check");
 };
-const changeInput = (classname,input) => {
+const changeInput = (classname, input) => {
   let x = document.querySelector(`#${classname}`);
   input.value.length > 0
     ? x.classList.add("inpt-focused")
-    : x.classList.remove("inpt-focused"); 
-    checkLoginBtn()
+    : x.classList.remove("inpt-focused");
+  checkLoginBtn();
 };
 
+changeBanner();
+//-------------------------------------------start valdation-------------------------------------------------------------
 
-changeBanner()
-
-
-
-
-
-
+//--------------------------------------------end valdtion------------------------------------------------------------
